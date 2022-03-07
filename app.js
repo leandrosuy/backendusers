4aa6a8971993ca2ef6fbdf06d31a0ea2a70d4c87
Leandro Dantas <leandrosuy@gmail.com>
2022-03-07 07:34:10 -0300

    Atualização de senha

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.get("/user/:id", async (req, res) => {
   if (!user) {
     return res.status(404).json({ msg: "Usuário não encontrado!" });
   }
+
+  res.status(200).json({ user });
 });
 
 app.post("/auth/register", async (req, res) => {
@@ -97,6 +99,25 @@ app.post("/auth/login", async (req, res) => {
     res
       .status(500)
       .json({ msg: "Aconteceu um erro no servidor, tente mais tarde!" });
+  }
+});
+
+app.patch("/user/:id", async (req, res) => {
+  const id = req.params.id;
+  const { password } = req.body;
+
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const user = {
+    password: passwordHash,
+  };
+
+  try {
+    const updateUser = await User.updateOne({ emai: id }, user);
+    res.status(200).json({ msg: "Senha alterada com sucesso" });
+  } catch (error) {
+    res.status(500).json({ error: error });
   }
 });
 
